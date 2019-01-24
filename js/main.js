c1e1afa7247b1ba6d9f0e365b921eb0987db52a7
TuEntrepiso <42011027+TuEntrepiso@users.noreply.github.com>
2019-01-24 13:40:02 -0300

    hamburger cross js

--- js/main.js
+++ js/main.js
@@ -53,12 +53,11 @@ function myFunction() {
 
 // Used to toggle the menu on small screens when clicking on the menu button
 function toggleFunction() {
+    var burger = document.getElementById("hamburgerContainer");
+    burger.classList.toggle('is-active');
+    
     var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-    }
+    x.classList.toggle('w3-show');
 }
 
 
