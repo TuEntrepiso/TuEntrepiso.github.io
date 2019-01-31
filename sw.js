00d0908ad1654aa74465606a99ea7d372b03ecaa
TuEntrepiso <42011027+TuEntrepiso@users.noreply.github.com>
2019-01-31 17:50:58 -0300

    try manifest

---
+++ sw.js
@@ -0,0 +1,70 @@
+var PRECACHE = 'precache-v1';
+var RUNTIME = 'runtime';
+
+// list the files you want cached by the service worker
+PRECACHE_URLS = [
+    'index.html',
+    'portfolio.html',
+    './',
+    'index.html?utm_source=homescreen',
+    'css/main-min.css',
+    'js/index-post-jq.js',
+    'js/portfolio-post-jq.js',
+    'js/js-index-loader.js',
+    'js/main-min.js',
+    'fotos/Entrepiso_1.jpg',
+    'fotos/Entrepiso_2.jpg',
+    'fotos/Entrepiso_3.jpg'
+
+];
+
+
+// the rest below handles the installing and caching
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(PRECACHE).then(cache => cache.addAll(PRECACHE_URLS)).then(self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+        }).then(cachesToDelete => {
+            return Promise.all(cachesToDelete.map(cacheToDelete => {
+                return caches.delete(cacheToDelete);
+            }));
+        }).then(() => self.clients.claim())
+    );
+});
+
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                var fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest).then(
+                    function(response) {
+                        // Check if we received a valid response
+                        if(!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        var responseToCache = response.clone();
+
+                        caches.open(RUNTIME)
+                            .then(function(cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
