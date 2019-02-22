4daa587b938b81aa0ac6f9dc8f57cd9629e7839b
Leonardo-Rocca <leonardo_7@live.com.ar>
2019-02-22 16:47:26 -0300

    backpress index-post-jq

--- js/index-post-jq.js
+++ js/index-post-jq.js
@@ -10,3 +10,9 @@ var MAX_ONSMALL_ELEMENTS = 4;
 
 loadImgs(1, FIRST_ELEMENTS);
 
+window.onpopstate = function(event) {
+    if (!$('#modal01').hidden) {
+        $('#modal01').hide();
+        $("#html").css('overflow-y', "scroll");
+    }
+};
